ec2445cc40b70a6fd00616f78ddfc6a9b8e94ebe
Harrison Jones <harrisonjones5678@gmail.com>
2024-07-14 19:13:57 -0400

    add img styles and containers for user details

--- index.js
+++ index.js
@@ -29,17 +29,31 @@ const setUpComment = (commentsObj) => {
     comments.forEach((comment) => {
         console.log("comment", comment);
         const mainCardDiv = document.createElement("div");
+        mainCardDiv.className = "card-main";
+        mainCardDiv.id = "card-main-container";
         const userNameSpan = document.createElement("span");
         userNameSpan.id = "username";
         userNameSpan.className = "username-of-comment-poster";
         userNameSpan.append(comment.user.username);
-        mainCardDiv.className = "card-main";
-        mainCardDiv.id = "card-main-container";
+        const createdOnSpan = document.createElement("span");
+        createdOnSpan.id = "created-on";
+        createdOnSpan.className = "created-on-time";
+        createdOnSpan.append(comment.createdAt);
+        const usersImage = document.createElement("img");
+        usersImage.src = comment.user.image.png;
+        usersImage.className = "users-image";
+        const cardUserContainer = document.createElement("div");
+        cardUserContainer.id = "user-info";
+        cardUserContainer.className = "user-info-container";
         const mainContainer = document.getElementById("main-page-container");
         mainContainer === null || mainContainer === void 0 ? void 0 : mainContainer.append(mainCardDiv);
         const getMainCardDiv = document.querySelectorAll(".card-main");
-        getMainCardDiv.forEach((cardDiv) => {
+        getMainCardDiv.forEach((cardDiv) => cardDiv.append(cardUserContainer));
+        const getUserInfoContainer = document.querySelectorAll(".user-info-container");
+        getUserInfoContainer.forEach((cardDiv) => {
+            cardDiv.append(usersImage);
             cardDiv.append(userNameSpan);
+            cardDiv.append(createdOnSpan);
         });
     });
 };
--- index.ts
+++ index.ts
@@ -18,7 +18,8 @@ interface comment {
   id: number;
   content: string;
   score: number;
-  user: { image: object; username: string };
+  user: { image: { png: string }; username: string };
+  createdAt: string;
 }
 
 const setUpComment = (commentsObj: Array<object>) => {
@@ -27,22 +28,42 @@ const setUpComment = (commentsObj: Array<object>) => {
   comments.forEach((comment: comment) => {
     console.log("comment", comment);
     const mainCardDiv = document.createElement("div");
+    mainCardDiv.className = "card-main";
+    mainCardDiv.id = "card-main-container";
 
     const userNameSpan = document.createElement("span");
-
     userNameSpan.id = "username";
     userNameSpan.className = "username-of-comment-poster";
     userNameSpan.append(comment.user.username);
 
-    mainCardDiv.className = "card-main";
-    mainCardDiv.id = "card-main-container";
+    const createdOnSpan = document.createElement("span");
+    createdOnSpan.id = "created-on";
+    createdOnSpan.className = "created-on-time";
+    createdOnSpan.append(comment.createdAt);
+
+    const usersImage = document.createElement("img");
+    usersImage.src = comment.user.image.png;
+    usersImage.className = "users-image";
+
+    const cardUserContainer = document.createElement("div");
+    cardUserContainer.id = "user-info";
+    cardUserContainer.className = "user-info-container";
+
     const mainContainer = document.getElementById("main-page-container");
     mainContainer?.append(mainCardDiv);
 
     const getMainCardDiv = document.querySelectorAll(".card-main");
 
-    getMainCardDiv.forEach((cardDiv) => {
+    getMainCardDiv.forEach((cardDiv) => cardDiv.append(cardUserContainer));
+
+    const getUserInfoContainer = document.querySelectorAll(
+      ".user-info-container"
+    );
+
+    getUserInfoContainer.forEach((cardDiv) => {
+      cardDiv.append(usersImage);
       cardDiv.append(userNameSpan);
+      cardDiv.append(createdOnSpan);
     });
   });
 };
